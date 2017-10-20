b0733c65abc5ccfa0d160b21d0882fa5dde86544
NGrondi <NGrondi@VNTG6350VS4.corp.ventia.com.au>
2017-10-20 15:03:32 +1100

    Contributions

--- Redback/src/com/nic/redback/js/mainmodule.js
+++ Redback/src/com/nic/redback/js/mainmodule.js
@@ -1,6 +1,6 @@
 	var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
 
-	function convertDateStringsToDates(input) {
+	function processResponseJSON(input) {
 		// Ignore things that aren't objects.
 		if (typeof input !== "object") return input;
 
@@ -9,15 +9,19 @@
 
 			var value = input[key];
 			var match;
-			// Check for string properties which look like dates.
-			if (typeof value === "string" && (match = value.match(regexIso8601))) {
-				var milliseconds = Date.parse(match[0])
-				if (!isNaN(milliseconds)) {
-					input[key] = new Date(milliseconds);
+			if (typeof value === "string") {
+				if(match = value.match(regexIso8601)) {
+					var milliseconds = Date.parse(match[0])
+					if (!isNaN(milliseconds)) {
+						input[key] = new Date(milliseconds);
+					}
 				}
+				else if(value == 'true')
+					input[key] = true;
+				else if(value == 'false')
+					input[key] = false;
 			} else if (typeof value === "object") {
-				// Recurse into object
-				convertDateStringsToDates(value);
+				processResponseJSON(value);
 			}
 		}
 	};
@@ -28,7 +32,7 @@
 		for (var key in relationship) {
 			var value = relationship[key];
 			if(value.startsWith("{{") &&  value.endsWith("}}")) {
-				var parentEval = 'object.' + value.substring(2, value.length - 2);
+				var parentEval = value.replace('{{', 'object.data.').replace('}}', '').replace('object.data.uid', 'object.uid');
 				value = eval(parentEval);
 			}
 			filter[key] = value;
@@ -44,37 +48,53 @@
 		$scope.objectName = $attrs.rbObject;
 		$scope.object = null;
 		$scope.dynamicSearchText = "";
+		$scope.relatedObjectList = {};
 		
 		$scope.setObject = function(object) {
 			$scope.object = object;
+			for (var key in object.related) {
+				$scope.relatedObjectList[key] = [object.related[key]];
+			}
 		}
 
 		
-		$scope.loadRelatedObjectList = function(attributeName)
+		$scope.loadRelatedObjectList = function(attributeName, searchText)
 		{
 			var relationship = $scope.object.validation[attributeName].relatedobject.relationship;
 			var relatedObjectName = $scope.object.validation[attributeName].relatedobject.name;
 			var filter = getFilterFromRelationship($scope.object, relationship)
-			filter._any = '*' + $scope.dynamicSearchText + '*';
+			if(searchText != null)
+				filter._any = '*' + searchText + '*';
 
-			var req = {action:"list", object:relatedObjectName, filter:filter};
+			var req = {action:"list", object:relatedObjectName, filter:filter, options:{addvalidation:"false"}};
 			return $http.post("../../rbos", req)
 				.then(function(response) {
+					processResponseJSON(response);
+					if(!$scope.relatedObjectList.hasOwnProperty(attributeName))
+						$scope.relatedObjectList[attributeName] = [];
+					$scope.relatedObjectList[attributeName] = response.data.list;
 					return response.data.list;
 				});
 		};
 		
 		$scope.relatedObjectHasChanged = function(attributeName)
 		{
-			var newSelectedItem = $scope.object.related[attributeName];
-			var newId = '';
-			if(newSelectedItem != null)
-				newId = $scope.object.related[attributeName].id;
-			if($scope.object.data[attributeName] != newId)
+			var newRelatedObject = $scope.object.related[attributeName];
+			var newLinkValue = '';
+			if(newRelatedObject != null)
 			{
-				$scope.object.data[attributeName] = newId;
+				var relatedObjectValueAttribute = $scope.object.validation[attributeName].relatedobject.valueattribute;
+				if(relatedObjectValueAttribute == 'uid')
+					newLinkValue = $scope.object.related[attributeName].uid;
+				else
+					newLinkValue = $scope.object.related[attributeName].data[relatedObjectValueAttribute];
+			}
+			if($scope.object.data[attributeName] != newLinkValue)
+			{
+				$scope.object.data[attributeName] = newLinkValue;
 			}
 		}
+
 		
 		$scope.$on('ObjectSelected', function($event, object){
 			if(object.objectname == $scope.objectName)
@@ -82,24 +102,46 @@
 		});
 
 		
-		$scope.save = function() {
-			var req = {action:"update", object:$scope.objectName, id:$scope.object.id, data:$scope.object.data, options:{addrelated:"true"}};
+
+		$scope.save = function(){
+			if($scope.object != null) {
+				var req = {action:"update", object:$scope.objectName, uid:$scope.object.uid, data:$scope.object.data, options:{addrelated:"true"}};
+				$http.post("../../rbos", req)
+				.success(function(response) {
+					processResponseJSON(response);
+					$scope.setObject(response);
+				})
+				.error(function(error, status) {
+					alert('save error');
+				});
+			}
+		};		
+
+
+		$scope.create = function(){
+			var req = {action:"create", object:$scope.objectName, options:{addrelated:"true"}};
 			$http.post("../../rbos", req)
 			.success(function(response) {
-				convertDateStringsToDates(response);
-				$scope.object = response;
+				processResponseJSON(response);
+				$scope.setObject(response);
+				$scope.$emit('refresh', $scope.objectName);
 			})
 			.error(function(error, status) {
-				alert('save error');
+				alert('create error');
 			});
-		}		
+		};		
+
 	 });
 	 
+
+
 	 
 	 
 	mainModule.controller('list', function accountsCtl($scope,$attrs,$http) {
 		$scope.list = null;
 		$scope.objectName = $attrs.rbObject;
+		$scope.searchText = "";
+
 		if($attrs.rbMaster != null)
 			$scope.masterConfig = JSON.parse($attrs.rbMaster.replace(/'/g, '"'));
 		else
@@ -110,12 +152,22 @@
 		else
 			$scope.filter == null;
 
+
+		$scope.search = function(searchText) {
+			if($scope.filter == null)
+				$scope.filter = {};
+			$scope.filter._any = '*' + $scope.searchText + '*';
+			$scope.load();
+		}
+
+
+
 		$scope.load = function() {
 			if($scope.filter != null) {
 				var req = {action:"list", object:$scope.objectName, filter:$scope.filter, options:{addrelated:"true"}};
 				$http.post("../../rbos", req)
 				.success(function(response) {
-					convertDateStringsToDates(response);
+					processResponseJSON(response);
 					$scope.list = response.list;
 				})
 				.error(function(error, status) {
@@ -123,14 +175,13 @@
 				});
 			}
 		}
-		
-		$scope.selectItem = function(object) {
-			$scope.$emit('ObjectSelectedEmit', object);
-		}
+
+
 		
 		$scope.$on('ObjectSelected', function($event, object){
 			if($scope.masterConfig != null && object.objectname == $scope.masterConfig.objectname)
 			{
+				$scope.searchText = "";
 				$scope.filter = getFilterFromRelationship(object, $scope.masterConfig.relationship)
 				$scope.load();
 			}
@@ -138,6 +189,9 @@
 
 		$scope.load();
 	 });	 
+
+
+
 	 
 	mainModule.controller('layout', function accountsCtl($scope,$attrs,$http) {
 
