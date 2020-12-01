0c34bcef378962eb9577f9dd13674fa6239b6522
Nicolas <ngrondin78@gmail.com>
2020-12-01 22:28:12 +1100

    Corrected date regression on graph component

--- RedbackNg/src/app/rb-graph/rb-graph.component.ts
+++ RedbackNg/src/app/rb-graph/rb-graph.component.ts
@@ -62,14 +62,13 @@ export class RbGraphComponent implements OnInit {
       let thisCat: String = this.categories != null ? agg.getDimension(this.categories.dimension) : null;
       if(cat == null || cat == thisCat) {
         let name: any = this.nullToEmptyString(agg.getDimension(this.series.labelattribute));
-        if(typeof name == 'string' && name.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})[+-](\d{2})\:(\d{2})/)) {
+        if(typeof name == 'string' && name.match(/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:.\d{1,9})?(?:Z|[+-][01]\d:[0-5]\d)$/)) {
           name = new Date(Date.parse(name));
         }
         let value = agg.getMetric(this.value.name);
         series.push({name: name, label: 'll', value: value});
       }
     }
-    //series.sort((a, b) => a > b ? 1 : -1);
     return series;
   }
 
