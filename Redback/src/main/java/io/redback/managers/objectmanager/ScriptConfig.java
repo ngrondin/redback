2baaf589191934a2b5ca8d125d8e4da01e14156b
Nicolas <ngrondin78@gmail.com>
2021-03-22 14:20:43 +1100

    Correction on the global script execution

--- Redback/src/main/java/io/redback/managers/objectmanager/ScriptConfig.java
+++ Redback/src/main/java/io/redback/managers/objectmanager/ScriptConfig.java
@@ -23,11 +23,15 @@ public class ScriptConfig
 		name = config.getString("name");
 		source = StringUtils.unescape(config.getString("script"));
 		List<String> scriptVars = new ArrayList<String>();
+		scriptVars.add("session");
+		scriptVars.add("userprofile");
+		scriptVars.add("firebus");
 		scriptVars.add("om");
 		scriptVars.add("pm");
-		scriptVars.add("firebus");
-		scriptVars.add("userprofile");
 		scriptVars.add("geo");
+		scriptVars.add("fc");
+		scriptVars.add("rc");
+		scriptVars.add("nc");
 		scriptVars.add("param");
 		function = new Function(jsm, name, scriptVars, source);
 	}
