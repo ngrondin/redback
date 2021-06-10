a69a1bd5d0d334e24fc9f39efa2b4be1e890445f
Nicolas <ngrondin78@gmail.com>
2021-06-10 16:53:27 +1000

    Change firebus statistics

--- Redback/src/main/java/io/redback/utils/Watchdog.java
+++ Redback/src/main/java/io/redback/utils/Watchdog.java
@@ -1,6 +1,9 @@
 package io.redback.utils;
 
+import java.util.List;
+
 import io.firebus.Firebus;
+import io.firebus.information.Statistics;
 
 public class Watchdog extends Thread {
 	
@@ -20,8 +23,12 @@ public class Watchdog extends Thread {
 				long totalHeap = (Runtime.getRuntime().totalMemory() / 1048576);
 				long freeHeap = (Runtime.getRuntime().freeMemory() / 1048576);
 				long usedHeap = totalHeap - freeHeap;
-				System.out.println("memory::" + System.currentTimeMillis() + ":" + usedHeap + ":" + totalHeap + ":" + maxHeap);
-				firebus.logStatus();
+				System.out.println("memory::" + System.currentTimeMillis() + ":" + usedHeap + "/" + totalHeap + "/" + maxHeap);
+				List<Statistics> stats = firebus.getStatistics();
+				StringBuilder sb = new StringBuilder();
+				for(Statistics stat : stats) 
+					sb.append(stat.toString());
+				System.out.println("firebus:" + sb.toString());
 				Thread.sleep(60000);
 			} catch(Exception e) {
 				e.printStackTrace();
