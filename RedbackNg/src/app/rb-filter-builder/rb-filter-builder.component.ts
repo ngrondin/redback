29319f7f52c2c5fd696cafe60d2e716d8fa8de77
Nicolas <ngrondin78@gmail.com>
2021-08-22 13:32:14 +1000

    Fix on UI filter builder

--- RedbackNg/src/app/rb-filter-builder/rb-filter-builder.component.ts
+++ RedbackNg/src/app/rb-filter-builder/rb-filter-builder.component.ts
@@ -325,7 +325,7 @@ export class RbFilterBuilderComponent implements OnInit {
         }
       }
       fltr = this.filterService.resolveFilter(fltr, null, null, null);
-      this.dataService.aggregateObjects(this.config.objectname, fltr, null, [fac.attribute], [{function:"count", name:"count"}], null).subscribe(list => {
+      this.dataService.aggregateObjects(this.config.objectname, fltr, null, [fac.attribute], [{function:"count", name:"count"}], null, 0, 100).subscribe(list => {
         fac.options = list.map(agg => {return {
           name: agg.getDimension(fac.attribute + "." + fac.displayAttribute), 
           value: agg.getDimension(fac.attribute),
