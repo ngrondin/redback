78f6032e0a10294b5f92046ed2bc9698db755e17
Nicolas <ngrondin78@gmail.com>
2021-09-24 13:42:08 +1000

    Change in logging

--- Redback/src/main/java/io/redback/managers/objectmanager/ObjectManager.java
+++ Redback/src/main/java/io/redback/managers/objectmanager/ObjectManager.java
@@ -397,7 +397,7 @@ public class ObjectManager
 								}
 								if(relatedObject == null) // Because of a broken link in the DB
 								{
-									logger.warning("Broken data link for object '" + objectConfig.getName() + (element instanceof RedbackObject ? ":" + ((RedbackObject)element).getUID().getString() : "") + "." + attributeName);
+									logger.info("Broken data link for object '" + objectConfig.getName() + (element instanceof RedbackObject ? ":" + ((RedbackObject)element).getUID().getString() : "") + "." + attributeName);
 									ObjectConfig zombieObjectConfig = getObjectConfig(session, relatedObjectConfig.getObjectName());
 									String zombieDBKey = (relatedObjectLinkAttributeName.equals("uid") ? zombieObjectConfig.getUIDDBKey() : zombieObjectConfig.getAttributeConfig(relatedObjectLinkAttributeName).getDBKey());
 									relatedObject = new RedbackObject(session, this, zombieObjectConfig, new DataMap(zombieDBKey, linkValue.getObject()));
