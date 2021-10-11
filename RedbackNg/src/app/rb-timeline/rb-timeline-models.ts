a1877399a1108db61b28b14a43c9acb5d18b6a09
Nicolas <ngrondin78@gmail.com>
2021-10-11 13:56:43 +1100

    Added more levels to timeline

--- RedbackNg/src/app/rb-timeline/rb-timeline-models.ts
+++ RedbackNg/src/app/rb-timeline/rb-timeline-models.ts
@@ -1,15 +1,22 @@
 import { SeriesConfig } from "app/abstract/rb-datacalc";
+import { RbObject } from "app/datamodel";
 
 export class TimelineSeriesConfig extends SeriesConfig {
     mainAttribute: string;
+    mainExpression: Function;
     subAttribute: string;
+    subExpression: Function;
     dateAttribute: string;
+    level: number;
   
     constructor(json: any) {
         super(json);
         this.mainAttribute = json.mainattribute;
+        this.mainExpression = json.mainexpression != null ? Function("object", "return (" + json.mainexpression + ");") : null;
         this.subAttribute = json.subattribute;
+        this.subExpression = json.subexpression != null ? Function("object", "return (" + json.subexpression + ");") : null;
         this.dateAttribute = json.dateattribute;
+        this.level = json.level || 0;
     }
   }
 
@@ -17,13 +24,16 @@ export class TimelineSeriesConfig extends SeriesConfig {
       date: Date;
       main: string;
       sub: string;
+      level: number;
       showDatePart: boolean = true;
+      showTimePart: boolean = true;
       showTopLine: boolean = true;
       showBottomLine: boolean = true;
 
-      constructor(d: Date, m: string, s: string) {
+      constructor(d: Date, m: string, s: string, l: number) {
           this.date = d;
           this.main = m;
           this.sub = s;
+          this.level = l;
       }
   }
