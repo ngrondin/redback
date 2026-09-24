f6c2a5b0ef2bed3f07db28c9a18fccafcbc5fe0b
Nicolas <ngrondin78@gmail.com>
2026-09-25 09:04:54 +1000

    Rebuilt client, added OutputStreamJSWrapper

--- Redback/src/main/java/io/redback/utils/js/OutputStreamJSWrapper.java
+++ Redback/src/main/java/io/redback/utils/js/OutputStreamJSWrapper.java
@@ -1,7 +1,9 @@
 package io.redback.utils.js;
 
+import java.io.ByteArrayInputStream;
 import java.io.OutputStream;
 
+import io.firebus.data.DataMap;
 import io.redback.exceptions.RedbackException;
 
 public class OutputStreamJSWrapper extends ObjectJSWrapper  {
@@ -13,6 +15,22 @@ public class OutputStreamJSWrapper extends ObjectJSWrapper  {
 	}
 
 	public Object get(String key) throws RedbackException {
+		if(key.equals("getCompletionData")) {
+			return new CallableJSWrapper() {
+				public Object call(Object... arguments) throws RedbackException {
+					if(outputStream instanceof io.firebus.utils.OutputStream) {
+						byte[] bytes = ((io.firebus.utils.OutputStream)outputStream).getCompletionBytes();
+						if(bytes != null) {
+							try {
+								DataMap data = new DataMap(new ByteArrayInputStream(bytes));
+								return data;
+							} catch(Exception e) { }
+						}
+					}
+					return null;
+				}
+			};
+		}
 		return null;
 	}
 
